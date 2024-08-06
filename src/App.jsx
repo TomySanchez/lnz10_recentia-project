d0db4e02823d894f30eebb2063843c6258a9b554
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-06 18:42:41 -0300

    feat: Configuración inicial de Ant Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { ConfigProvider } from 'antd';
 import { Routes } from './routes/Routes';
+import esEs from 'antd/locale/es_ES';
+import { colorsPalette } from './utils/colorsPalette';
 
 function App() {
+  const { primaryColor, lightColor, white } = colorsPalette;
+
   return (
-    <div className='App'>
-      <Routes />
-    </div>
+    <ConfigProvider
+      locale={esEs}
+      theme={{
+        token: {
+          colorPrimary: primaryColor,
+          colorPrimaryBg: lightColor,
+          colorBgLayout: white
+        }
+      }}
+    >
+      <div className='App'>
+        <Routes />
+      </div>
+    </ConfigProvider>
   );
 }
 
---
+++ src/utils/colorsPalette.js
@@ -0,0 +1,11 @@
+import { cyan } from '@ant-design/colors';
+
+export const colorsPalette = {
+  primaryColor: cyan[7],
+  darkColor: cyan[9],
+  darkMediumColor: cyan[8],
+  lightColor: cyan[1],
+  lightMediumColor: cyan[2],
+  lightExtraColor: '#edfdfa',
+  white: '#ffffff'
+};
