e02509ad874bf983485c5fb1553c7c7195343b78
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-06 18:45:39 -0300

    feat: JSONs de datos

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { DataProvider } from './contexts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </Router>
   </React.StrictMode>
 );
