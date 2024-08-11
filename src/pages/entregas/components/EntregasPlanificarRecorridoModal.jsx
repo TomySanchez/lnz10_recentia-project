0c3b61ea70d7280e82b012ff98d3aa8998b3d312
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-11 16:35:05 -0300

    feat[entregas]: Planificador de recorrido

--- src/pages/entregas/components/EntregasPlanificarRecorridoModal.jsx
+++ src/pages/entregas/components/EntregasPlanificarRecorridoModal.jsx
@@ -1,6 +1,32 @@
-import { Modal } from 'antd';
+import { useContext, useState } from 'react';
+import { DatePicker, Form, Modal, Table } from 'antd';
+import { DataContext } from '../../../contexts';
+import { getItemById } from '../../../utils';
 
 export const EntregasPlanificarRecorridoModal = ({ open, setOpen }) => {
+  const { entregas } = useContext(DataContext);
+
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  const planificadorForm = Form.useForm();
+
+  const entregasNoProgramadas = entregas.filter(
+    (entrega) => entrega.estado === 'Sin programar'
+  );
+
+  const entregasNoProgramadasColumns = [
+    {
+      dataIndex: 'idPedido',
+      title: 'Cliente',
+      render: (text) => {
+        const pedido = getItemById(text, 'pedido');
+        const cliente = getItemById(pedido.idCliente, 'cliente');
+
+        return cliente.nombre;
+      }
+    }
+  ];
+
   function handleOk() {
     setOpen(false);
   }
@@ -16,6 +42,33 @@ export const EntregasPlanificarRecorridoModal = ({ open, setOpen }) => {
       onOk={handleOk}
       onCancel={handleCancel}
       destroyOnClose
-    ></Modal>
+    >
+      <Form form={planificadorForm} name='planificadorForm' layout='vertical'>
+        <Form.Item
+          name='fecha'
+          label='Fecha del recorrido'
+          rules={[
+            {
+              required: true,
+              message: 'Fecha requerida'
+            }
+          ]}
+        >
+          <DatePicker />
+        </Form.Item>
+      </Form>
+
+      <Table
+        rowKey='id'
+        size='small'
+        rowSelection={{
+          selectedRowKeys,
+          onChange: setSelectedRowKeys
+        }}
+        columns={entregasNoProgramadasColumns}
+        dataSource={entregasNoProgramadas}
+        pagination={false}
+      />
+    </Modal>
   );
 };
