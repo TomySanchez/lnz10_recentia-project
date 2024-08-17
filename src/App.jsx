86fef42d2b941b99c3219d0de74ea4245b30ac8a
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-17 11:31:08 -0300

    feat: Enrutamiento inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Routes } from './routes/Routes';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,32 @@
+import { Navigate, Route, Routes as RoutesRR } from 'react-router-dom';
+import { ProtectedRoute } from './ProtectedRoute';
+import { isAuthenticated } from './isAuthenticated';
+
+export const Routes = () => {
+  return (
+    <RoutesRR>
+      {/* <Route path='/inicio' element={<Login />} /> */}
+      <Route
+        path='/'
+        element={
+          isAuthenticated() ? (
+            <Navigate replace to='/clientes' />
+          ) : (
+            <Navigate replace to='/inicio' />
+          )
+        }
+      />
+      <Route
+        path='/'
+        element={<ProtectedRoute>{/* <MainLayout /> */}</ProtectedRoute>}
+      >
+        {/* <Route path='clientes' element={<Clientes />} /> */}
+        {/* <Route path='pedidos' element={<Pedidos />} /> */}
+        {/* <Route path='entregas' element={<Entregas />} /> */}
+        {/* <Route path='pagos' element={<Pagos />} /> */}
+        {/* <Route path='configuracion' element={<Configuracion />} /> */}
+      </Route>
+      {/* <Route path='*' element={<NotFound />} /> */}
+    </RoutesRR>
+  );
+};
