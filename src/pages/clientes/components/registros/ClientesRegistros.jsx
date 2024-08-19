9a2147cc4cb76c7e6668453e52652f4fda1f6828
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-18 22:44:10 -0300

    feat[clientes]: Botón Volver en Registros de un cliente

--- src/pages/clientes/components/registros/ClientesRegistros.jsx
+++ src/pages/clientes/components/registros/ClientesRegistros.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { MdOutlinePerson } from 'react-icons/md';
 import { MainContent } from '../../../../layouts/MainContent';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ClientesChangeRegistro } from './ClientesChangeRegistro';
 import { RegistrosPedidos } from './RegistrosPedidos';
 import { RegistrosEntregas } from './RegistrosEntregas';
+import { Button } from 'antd';
 
 export const ClientesRegistros = () => {
+  const navigateTo = useNavigate();
   const location = useLocation();
   const cliente = location.state?.cliente;
 
@@ -21,10 +23,21 @@ export const ClientesRegistros = () => {
       title={`Registros de ${cliente.nombre}`}
       icon={<MdOutlinePerson size={40} />}
       extra={
-        <ClientesChangeRegistro
-          selectedRegistros={selectedRegistros}
-          setSelectedRegistros={setSelectedRegistros}
-        />
+        <div className='clientes-registros-extra-container'>
+          <ClientesChangeRegistro
+            selectedRegistros={selectedRegistros}
+            setSelectedRegistros={setSelectedRegistros}
+          />
+
+          <Button
+            className='clientes-registros-volver-button'
+            // size='large'
+            // type='primary'
+            onClick={() => navigateTo('/clientes')}
+          >
+            Volver
+          </Button>
+        </div>
       }
     >
       {selectedRegistros === 'Pedidos' ? (
