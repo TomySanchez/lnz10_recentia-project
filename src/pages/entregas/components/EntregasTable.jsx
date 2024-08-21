859aa9285f250f4bc9d60920eda6661bb9a5add3
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-20 21:27:24 -0300

    feat[entregas]: Monto total y Estado de pago en tabla de Entregas (subtabla de Recorridos)

--- src/pages/entregas/components/EntregasTable.jsx
+++ src/pages/entregas/components/EntregasTable.jsx
@@ -3,8 +3,13 @@ import { getItemById } from '../../../utils/getItemById';
 import { Acciones } from '../../../components/tables/Acciones';
 import { Table } from '../../../components/tables/Table';
 import { getMultipleItemsById } from '../../../utils/getMultipleItemsById';
+import { getDetalles } from '../../../utils/getDetalles';
+import { useContext } from 'react';
+import { DataContext } from '../../../contexts';
 
 export const EntregasTable = ({ className, recorrido, onInfo, onEdit }) => {
+  const { pagos } = useContext(DataContext);
+
   const entregasColumns = [
     {
       dataIndex: 'idPedido',
@@ -17,8 +22,30 @@ export const EntregasTable = ({ className, recorrido, onInfo, onEdit }) => {
       }
     },
     {
+      dataIndex: 'id',
+      title: 'Monto total',
+      render: (text) => {
+        const pagoDeEntrega = pagos.find((pago) => pago.idEntrega == text);
+        const detallesDePago = getDetalles(pagoDeEntrega.id, 'pagos');
+        const importes = detallesDePago.map((detalle) => {
+          const precio = getItemById(detalle.idPrecio, 'precio').valor;
+          const cantidad = getItemById(
+            detalle.idDetalleDeEntrega,
+            'detalleDeEntrega'
+          ).cantidad;
+
+          return precio * cantidad;
+        });
+        const importeTotal = importes.reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0
+        );
+        return `$ ${importeTotal}`;
+      }
+    },
+    {
       dataIndex: 'estado',
-      title: 'Estado',
+      title: 'Estado de entrega',
       align: 'center',
       render: (text) => {
         let colorTag;
@@ -37,6 +64,28 @@ export const EntregasTable = ({ className, recorrido, onInfo, onEdit }) => {
         }
 
         return <Tag color={colorTag}>{text}</Tag>;
+      }
+    },
+    {
+      dataIndex: 'id',
+      title: 'Estado de pago',
+      align: 'center',
+      render: (text) => {
+        const pagoDeEntrega = pagos.find((pago) => pago.idEntrega == text);
+
+        let colorTag;
+        switch (pagoDeEntrega.estado) {
+          case 'Pendiente':
+            colorTag = 'gold';
+            break;
+          case 'Pagado':
+            colorTag = 'green';
+            break;
+          default:
+            colorTag = '';
+        }
+
+        return <Tag color={colorTag}>{pagoDeEntrega.estado}</Tag>;
       }
     },
     {
