37b0d122a7eb23d43c9b4dd9d291f97bd1914292
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-21 15:38:08 -0300

    feat[clientes]: Filtros de tabla de clientes

--- src/pages/clientes/components/ClientesTable.jsx
+++ src/pages/clientes/components/ClientesTable.jsx
@@ -3,22 +3,59 @@ import { useContext } from 'react';
 import { DataContext } from '../../../contexts';
 import { Table } from '../../../components/tables/Table';
 import { Acciones } from '../../../components/tables/Acciones';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BuscadorTabla } from '../../../components/tables/BuscadorTabla';
 
 export const ClientesTable = ({ onInfo, onEdit }) => {
-  const { clientes } = useContext(DataContext);
+  const { clientes, barrios } = useContext(DataContext);
 
   const clientesColumns = [
     {
       dataIndex: 'nombre',
       title: 'Nombre',
-      sorter: (rowA, rowB) => rowA.nombre.localeCompare(rowB.nombre)
+      sorter: (rowA, rowB) => rowA.nombre.localeCompare(rowB.nombre),
+      filterIcon: () => <AiOutlineSearch />,
+      onFilter: (value, record) =>
+        record.nombre.toLowerCase().trim().includes(value.toLowerCase().trim()),
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters
+      }) => (
+        <BuscadorTabla
+          setSelectedKeys={setSelectedKeys}
+          selectedKeys={selectedKeys}
+          confirm={confirm}
+          clearFilters={clearFilters}
+        />
+      )
     },
     {
       dataIndex: 'idDireccion',
       title: 'Dirección',
       render: (_, record) => formatDireccion(record.direccion),
       sorter: (rowA, rowB) =>
-        rowA.direccion.calle.localeCompare(rowB.direccion.calle)
+        rowA.direccion.calle.localeCompare(rowB.direccion.calle),
+      filterIcon: () => <AiOutlineSearch />,
+      onFilter: (value, record) =>
+        formatDireccion(record.direccion)
+          .toLowerCase()
+          .trim()
+          .includes(value.toLowerCase().trim()),
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters
+      }) => (
+        <BuscadorTabla
+          setSelectedKeys={setSelectedKeys}
+          selectedKeys={selectedKeys}
+          confirm={confirm}
+          clearFilters={clearFilters}
+        />
+      )
     },
     {
       dataIndex: 'barrio',
@@ -26,7 +63,12 @@ export const ClientesTable = ({ onInfo, onEdit }) => {
       render: (_, record) =>
         `${record.barrio.nombre}, ${record.localidad.nombre}`,
       sorter: (rowA, rowB) =>
-        rowA.barrio.nombre.localeCompare(rowB.barrio.nombre)
+        rowA.barrio.nombre.localeCompare(rowB.barrio.nombre),
+      filters: barrios.map((barrio) => ({
+        text: barrio.nombre,
+        value: barrio.nombre
+      })),
+      onFilter: (value, record) => record.barrio.nombre === value
     },
     {
       dataIndex: 'observaciones',
