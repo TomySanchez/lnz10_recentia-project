a1c2764a674a4f00f19d9d54c0739189f14cba84
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-08-23 16:00:59 -0300

    feat[entregas]: Interfaz mobile

--- src/pages/entregas/Entregas.jsx
+++ src/pages/entregas/Entregas.jsx
@@ -2,12 +2,17 @@ import { MdOutlineLocalShipping } from 'react-icons/md';
 import { MainContent } from '../../layouts/MainContent';
 import { RecorridosTable } from './components/RecorridosTable';
 import { RecorridosPlanificarButton } from './components/RecorridosPlanificarButton';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { RecorridosPlanificarModal } from './components/RecorridosPlanificarModal';
 import { RecorridosDrawer } from './components/RecorridosDrawer';
 import { EntregasDrawer } from './components/EntregasDrawer';
+import { EntregasMobile } from './components/EntregasMobile';
+import { ResponsiveContext } from '../../contexts/ResponsiveContext';
+import { MobileContent } from '../../layouts/MobileContent';
 
 export const Entregas = () => {
+  const windowWidth = useContext(ResponsiveContext);
+
   const [openPlanificarRecorridoModal, setOpenPlanificarRecorridoModal] =
     useState(false);
   const [drawerModeRecorrido, setDrawerModeRecorrido] = useState('info');
@@ -49,38 +54,49 @@ export const Entregas = () => {
   }
 
   return (
-    <MainContent
-      title='Entregas'
-      icon={<MdOutlineLocalShipping size={40} />}
-      extra={<RecorridosPlanificarButton onAdd={handleAddRecorrido} />}
-    >
-      <h3 className='recorridos-title'>Tabla de recorridos</h3>
-      <RecorridosTable
-        onInfoRecorrido={handleInfoRecorrido}
-        onEditRecorrido={handleEditRecorrido}
-        onInfoEntrega={handleInfoEntrega}
-        onEditEntrega={handleEditEntrega}
-      />
-      <RecorridosPlanificarModal
-        mode={planificadorRecorridoMode}
-        recorrido={selectedRecorrido}
-        openModal={openPlanificarRecorridoModal}
-        setOpenModal={setOpenPlanificarRecorridoModal}
-      />
-      <RecorridosDrawer
-        mode={drawerModeRecorrido}
-        recorrido={selectedRecorrido}
-        open={openDrawerRecorrido}
-        setOpen={setOpenDrawerRecorrido}
-        onInfoEntrega={handleInfoEntrega}
-        onEditEntrega={handleEditEntrega}
-      />
-      <EntregasDrawer
-        mode={drawerModeEntrega}
-        entrega={selectedEntrega}
-        open={openDrawerEntrega}
-        setOpen={setOpenDrawerEntrega}
-      />
-    </MainContent>
+    <>
+      {windowWidth > 700 ? (
+        <MainContent
+          title='Entregas'
+          icon={<MdOutlineLocalShipping size={40} />}
+          extra={<RecorridosPlanificarButton onAdd={handleAddRecorrido} />}
+        >
+          <h3 className='recorridos-title'>Tabla de recorridos</h3>
+          <RecorridosTable
+            onInfoRecorrido={handleInfoRecorrido}
+            onEditRecorrido={handleEditRecorrido}
+            onInfoEntrega={handleInfoEntrega}
+            onEditEntrega={handleEditEntrega}
+          />
+          <RecorridosPlanificarModal
+            mode={planificadorRecorridoMode}
+            recorrido={selectedRecorrido}
+            openModal={openPlanificarRecorridoModal}
+            setOpenModal={setOpenPlanificarRecorridoModal}
+          />
+          <RecorridosDrawer
+            mode={drawerModeRecorrido}
+            recorrido={selectedRecorrido}
+            open={openDrawerRecorrido}
+            setOpen={setOpenDrawerRecorrido}
+            onInfoEntrega={handleInfoEntrega}
+            onEditEntrega={handleEditEntrega}
+          />
+          <EntregasDrawer
+            mode={drawerModeEntrega}
+            entrega={selectedEntrega}
+            open={openDrawerEntrega}
+            setOpen={setOpenDrawerEntrega}
+          />
+        </MainContent>
+      ) : (
+        <MobileContent
+          title='Entregas'
+          icon={<MdOutlineLocalShipping size={50} />}
+        >
+          <EntregasMobile />
+        </MobileContent>
+      )}
+    </>
   );
 };
---
+++ src/pages/entregas/components/EntregasMobile.jsx
@@ -0,0 +1,71 @@
+import { Button } from 'antd';
+import { useContext, useState } from 'react';
+import { DataContext } from '../../../contexts';
+import { getMultipleItemsById } from '../../../utils/getMultipleItemsById';
+import { getItemById } from '../../../utils/getItemById';
+import { Table } from '../../../components/tables/Table';
+import { Acciones } from '../../../components/tables/Acciones';
+
+export const EntregasMobile = () => {
+  const { recorridos } = useContext(DataContext);
+
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  const recorridoActual = recorridos[7];
+
+  const entregasActuales = getMultipleItemsById(recorridoActual.id, 'entregas');
+
+  const datosEntrega = entregasActuales.map((entrega) => {
+    const pedido = getItemById(entrega.idPedido, 'pedido');
+    const cliente = getItemById(pedido.idCliente, 'cliente');
+
+    return {
+      nombreCliente: cliente.nombre
+    };
+  });
+
+  const entregasColumns = [
+    {
+      dataIndex: 'nombreCliente',
+      title: 'Nombre'
+    },
+    {
+      dataIndex: '',
+      title: '',
+      render: () => <Acciones />
+    }
+  ];
+
+  return (
+    <div className='EntregasMobile'>
+      <div className='entregas-mobile-title'>
+        <h3>Recorrido de hoy</h3>
+        <h3>({recorridoActual.fecha})</h3>
+      </div>
+
+      {/* <Button className='entregas-mobile-button' size='large' type='primary'>
+        Iniciar recorrido
+      </Button> */}
+
+      {/* {datosEntrega.map((entrega, index) => (
+        <Checkbox key={index}>{entrega.nombreCliente}</Checkbox>
+      ))} */}
+
+      <Table
+        className='entregas-mobile-table'
+        size='small'
+        rowSelection={{
+          selectedRowKeys,
+          onChange: setSelectedRowKeys
+        }}
+        columns={entregasColumns}
+        dataSource={datosEntrega}
+        pagination={false}
+      />
+
+      <Button className='entregas-mobile-button' size='large' type='primary'>
+        Finalizar recorrido
+      </Button>
+    </div>
+  );
+};
