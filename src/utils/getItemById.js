4542807520469ebcf6aa18c45a8c91e551ad6d50
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-10-05 17:08:09 -0300

    fix: getItemById no busca en items traídos desde la API

--- src/utils/getItemById.js
+++ src/utils/getItemById.js
@@ -14,53 +14,61 @@ import {
   dataRecorridos
 } from '../data';
 
-export function getItemById(id, itemType) {
+export function getItemById(id, itemType_itemsArray) {
   let data;
-  switch (itemType) {
-    case 'barrio':
-      data = dataBarrios;
-      break;
-    /* case 'cliente':
-      data = dataClientes;
-      break; */
-    case 'detalleDeEntrega':
-      data = dataDetallesDeEntregas;
-      break;
-    case 'detalleDePedido':
-      data = dataDetallesDePedidos;
-      break;
-    case 'detalleDePago':
-      data = dataDetallesDePagos;
-      break;
-    case 'direccion':
-      data = dataDirecciones;
-      break;
-    case 'entrega':
-      data = dataEntregas;
-      break;
-    case 'localidad':
-      data = dataLocalidades;
-      break;
-    case 'metodoDePago':
-      data = dataMetodosDePago;
-      break;
-    case 'pago':
-      data = dataPagos;
-      break;
-    case 'pedido':
-      data = dataPedidos;
-      break;
-    case 'precio':
-      data = dataPrecios;
-      break;
-    case 'producto':
-      data = dataProductos;
-      break;
-    case 'recorrido':
-      data = dataRecorridos;
-      break;
-    default:
-      data = null;
+
+  /* TODO Condicional temporal. Una vez haya reemplazado todos los JSONs por datos de API eliminaré el condicional, quedando solo el contenido de condición se cumple. También cambiaré el nombre de 'itemType_itemsArray' a 'itemsArray'. */
+  if (Array.isArray(itemType_itemsArray)) {
+    const item = itemType_itemsArray.find((i) => i.id == id);
+
+    return item;
+  } else {
+    switch (itemType_itemsArray) {
+      case 'barrio':
+        data = dataBarrios;
+        break;
+      /* case 'cliente':
+        data = dataClientes;
+        break; */
+      case 'detalleDeEntrega':
+        data = dataDetallesDeEntregas;
+        break;
+      case 'detalleDePedido':
+        data = dataDetallesDePedidos;
+        break;
+      case 'detalleDePago':
+        data = dataDetallesDePagos;
+        break;
+      case 'direccion':
+        data = dataDirecciones;
+        break;
+      case 'entrega':
+        data = dataEntregas;
+        break;
+      case 'localidad':
+        data = dataLocalidades;
+        break;
+      case 'metodoDePago':
+        data = dataMetodosDePago;
+        break;
+      case 'pago':
+        data = dataPagos;
+        break;
+      case 'pedido':
+        data = dataPedidos;
+        break;
+      case 'precio':
+        data = dataPrecios;
+        break;
+      case 'producto':
+        data = dataProductos;
+        break;
+      case 'recorrido':
+        data = dataRecorridos;
+        break;
+      default:
+        data = null;
+    }
   }
 
   if (data) {
@@ -68,6 +76,6 @@ export function getItemById(id, itemType) {
 
     return item;
   } else {
-    console.error('itemType no es válido:', itemType);
+    console.error('itemType_itemsArray no es válido:', itemType_itemsArray);
   }
 }
