1fa8d828a0e9e4fb23039738ddec38736bdc019d
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-10-05 21:11:34 -0300

    feat[clientes]: Call API para añadir cliente

--- src/services/clientes.js
+++ src/services/clientes.js
@@ -13,3 +13,18 @@ export async function getClientes() {
       throw err;
     });
 }
+
+export async function addCliente(data) {
+  const config = {
+    method: 'post',
+    url: `${import.meta.env.VITE_API_URL}/clientes`,
+    data: data
+  };
+
+  return await axios(config)
+    .then((res) => res)
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+}
