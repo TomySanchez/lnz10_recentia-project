e07ea667a251f999aea9b0f51a654a5f6f013373
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-11-09 18:45:47 -0300

    feat[clientes]: Mostrar disponibilidad en Más información

--- src/pages/clientes/components/ClientesInfoDrawer.jsx
+++ src/pages/clientes/components/ClientesInfoDrawer.jsx
@@ -1,5 +1,6 @@
 import { Descriptions } from 'antd';
 import { formatDireccion } from '../../../utils/formatDireccion';
+import dayjs from 'dayjs';
 
 const { Item } = Descriptions;
 
@@ -10,6 +11,24 @@ export const ClientesInfoDrawer = ({ cliente }) => {
       <Item label='Dirección'>{`${formatDireccion(cliente.direccion)}, ${
         cliente.direccion.barrio
       }, ${cliente.direccion.localidad}`}</Item>
+      <Item label='Disponibilidad'>
+        {cliente.disponibilidades && cliente.disponibilidades.length > 0 ? (
+          <div>
+            {cliente.disponibilidades.map((disp) => (
+              <div key={disp.idDisponibilidad} style={{ marginBottom: 8 }}>
+                {`${disp.diaSemana}: ${dayjs(
+                  disp.horaInicio,
+                  'HH:mm:ss'
+                ).format('HH:mm')} - ${dayjs(disp.horaFin, 'HH:mm:ss').format(
+                  'HH:mm'
+                )}`}
+              </div>
+            ))}
+          </div>
+        ) : (
+          '-'
+        )}
+      </Item>
       <Item label='Teléfono'>{cliente.telefono || '-'}</Item>
       <Item label='CUIT/CUIL'>{cliente.cuit_cuil || '-'}</Item>
       <Item label='Observaciones'>{cliente.observaciones || '-'}</Item>
